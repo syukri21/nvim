f7c6dca28516b2993db2be850eb99329d8f6b78a
Syukri Husaibatul Khairi HSB <syukri.khairi@Syukri-Hsb.local>
2025-02-11 02:17:20 +0700

    ok

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,16 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--
+-- Set tab size to 4 spaces
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = 4
+
+o.expandtab = true
+
+vim.cmd [[filetype plugin indent on]]
+
+o.smartindent = true
