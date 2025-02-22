4d1ded1e2c3f3e8123f7df080aa2e0235014397a
syukri21 <syukrihsb148@gmail.com>
2025-02-23 03:27:56 +0700

    fix treesitter

--- lua/configs/treesitter.lua
+++ lua/configs/treesitter.lua
@@ -14,7 +14,7 @@ ts.setup {
   },
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = true,
+    additional_vim_regex_highlighting = false,
   },
   textobjects = {
     move = {
